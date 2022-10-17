95b0406f7e4b84e10e616fcacd4f6eec98de1999
JOSE ELTON <115085872+eltonsilva23@users.noreply.github.com>
2022-10-17 20:06:23 +0000

    Exercícios

--- .ccls-cache/@home@runner@Algoritmos/main.c
+++ .ccls-cache/@home@runner@Algoritmos/main.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
-int main(){
-  int m1,m2,horas,minutos;
-  scanf("%d", &m1);
-  scanf("%d", &m2);
-  horas = (m2 - m1)/60;
-  minutos = (m2 - m1)%60;
-  printf("%02d:%d\n", horas, minutos);
+int main() {
+  long long a11,a12;
+  scanf("%lld %lld", &a11, &a12);
+  long long a21,a22;
+  scanf("%lld %lld", &a21, &a22);
+  long long p1,p2;
+  scanf("%lld %lld", &p1, &p2);
+  long long m1, m2;
+  m1=(a11*p1+a12*p2)/(p1+p2);
+  m2=(a21*p1+a22*p2)/(p1+p2);
+  if (m1>=m2) {printf("1\n");}
+  else {printf("2\n");}
   return 0;
 }
---
+++ Lista_1/q_G.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+  long long a11,b12;
+  scanf("%lld %lld", &a11, &b12);
+  long long a21,a22;
+  scanf("%lld %lld", &a21, &a22);
+  long long p1,p2;
+  scanf("%lld %lld", &p1, &p2);
+  return 0;
+}
